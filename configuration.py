64583b5e266cc654b3b929ba17972f9820df02be
kaithaberzus <k9609806349@gmail.com>
2025-03-22 02:15:05 +0700

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-main_link='https://70abc48c-b304-4698-afe9-df92879b46c6.serverhub.praktikum-services.ru/'
+main_link='https://70abc48c-b304-4698-afe9-df92879b46c6.serverhub.praktikum-services.ru'
 create_order="/api/v1/orders"
 get_order_by_track="/api/v1/orders/track"
