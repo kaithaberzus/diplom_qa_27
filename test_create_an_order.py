354589fc32abc3cd8724afeba80588fc4ed775d6
kaithaberzus <k9609806349@gmail.com>
2025-03-22 02:14:48 +0700

    Update test_create_an_order.py

--- test_create_an_order.py
+++ test_create_an_order.py
@@ -1,4 +1,5 @@
 import sender_stand_requests
-def test_get_new_user_token_by_track():
-    order_response=sender_stand_requests.get_new_order_by_track(track=sender_stand_requests.track)
-    assert order_response.status_code==200
+def test_get_order_code_200(order_track):
+    order_track = sender_stand_requests.get_new_order_track().json()['track']
+    response_get = sender_stand_requests.get_new_order_by_track(order_track)  
+    assert response_get.status_code == 200
