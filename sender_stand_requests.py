3f851937a05f369ef19c38f3a872a92864256efa
kaithaberzus <k9609806349@gmail.com>
2025-03-22 02:18:34 +0700

    Update sender_stand_requests.py

--- sender_stand_requests.py
+++ sender_stand_requests.py
@@ -4,7 +4,5 @@ import configuration
 def get_new_order_track():
     return requests.post(configuration.MAIN_LINK+configuration.CREATE_ORDER, 
                          json=data.order_structure)
-response=get_new_order_track()
-print(response.json()["track"])
 def get_new_order_by_track(order_track):
     return requests.get(configuration.MAIN_LINK+configuration.GET_ORDER_BY_TRACK, params={"t":order_track})
